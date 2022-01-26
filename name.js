e7fb95aa868e00a5d2d78ee3fbdaad40d28089bc
DJCactusBlue <89181973+DJCactusBlue@users.noreply.github.com>
2022-01-26 01:04:12 -0700

    rewritten most

--- name.js
+++ name.js
@@ -1,21 +1,4 @@
-// Color underline tabs
 
-function openGenre(evt, genreName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(genreName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
 
 // Slideshow
 var slideIndex = 1;
